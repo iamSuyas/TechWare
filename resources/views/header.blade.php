81afb0da165a60882624a3a0357da8a6b7bd86a2
Suyash Adhikari <adhikarisuyash23@gmail.com>
2023-07-19 20:43:19 +0545

    header,footer and login page done

---
+++ resources/views/header.blade.php
@@ -0,0 +1,42 @@
+<nav class="navbar  border-bottom ">
+  <div class="container">
+    <a class="navbar-brand" href="#">TechWare</a>
+      <ul class="navbar-nav d-flex flex-row navbar-center mb-2 mb-lg-0 gap-5">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#">Deals</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Brands</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#">Categories</a>
+        </li>
+        
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Accessories</a>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right d-flex flex-row gap-3">
+        <li>
+            <a href="#" class="nav-link">Cart(0)</a>
+        </li>
+        <li>
+            <a href="#" class="nav-link">Account</a>
+        </li>
+      </ul>
+  </div>
+</nav>
+
+<nav class="navbar border-bottom bg-light">
+  <div class="container d-flex justify-content-center">
+      <form class="d-flex gap-3" role="search">
+        <input class="form-control  col-lg-4 circle" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> 
+  </div>
+</nav>
+
