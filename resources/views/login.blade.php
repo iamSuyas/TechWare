254e16250463a5dc749dab4deffc19de54ed7149
Suyash Adhikari <adhikarisuyash23@gmail.com>
2023-07-24 08:51:25 +0545

    login with session complete

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,18 +3,19 @@
 <div class="container login-space">
     <div class="row d-flex justify-content-center">
         <div class="col-sm-4">
-        <form>
+        <form action="login" method="POST">
+          @csrf
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="email" name="email"class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" name="password"class="form-control" id="exampleInputPassword1">
   </div>
   
-  <button type="submit" class="btn btn-outline-default border">Submit</button>
+  <button type="submit" class="btn btn-outline-default border">Login</button>
 </form>
         </div>
     </div>
